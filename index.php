7f05fc3ecb4ea3403ca4c767f12a1c529260f614
dikshalambe12 <lambediksha417gmail.com>
2024-04-29 00:53:43 +0530

    add readme file

--- index.php
+++ index.php
@@ -1,2 +1,6 @@
 <!-- git init --><!-- for making git repo-->
-<p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Atque architecto, sunt saepe esse dignissimos dolorem unde quaerat eum, officiis repellat impedit corrupti est quas suscipit, sit molestiae. Repellat, eius alias.</p>
+<!-- git branch -m <branch name> --><!-- main or master -->
+<!-- git push -u origin main--> <!--it means we have do work on one project for long time that time we will 
+use -u in this command and then later we are use only (git push) -->
+<p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Atque architecto, sunt saepe esse dignissimos dolorem unde
+    quaerat eum, officiis repellat impedit corrupti est quas suscipit, sit molestiae. Repellat, eius alias.</p>
