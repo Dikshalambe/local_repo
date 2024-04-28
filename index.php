f6364a76611a971336e899e85110f4234666df68
dikshalambe12 <lambediksha417gmail.com>
2024-04-29 00:56:17 +0530

    add comments

--- index.php
+++ index.php
@@ -2,5 +2,6 @@
 <!-- git branch -m <branch name> --><!-- main or master -->
 <!-- git push -u origin main--> <!--it means we have do work on one project for long time that time we will 
 use -u in this command and then later we are use only (git push) -->
+<!-- add one file readme.md -->
 <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Atque architecto, sunt saepe esse dignissimos dolorem unde
     quaerat eum, officiis repellat impedit corrupti est quas suscipit, sit molestiae. Repellat, eius alias.</p>
